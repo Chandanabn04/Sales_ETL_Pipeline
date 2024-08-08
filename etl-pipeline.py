49e8af9f20cced181363c00e2846ee5ac4d7cf07
Chandanabn04 <chandanashakunthala04@gmail.com>
2024-08-08 16:36:08 -0400

    Update etl-pipeline.py

--- etl-pipeline.py
+++ etl-pipeline.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from sqlalchemy import create_engine, Table, MetaData, Column
 from sqlalchemy.types import Integer, String, Numeric, Date
+from sqlalchemy.exc import SQLAlchemyError
 
+file_path='your_file_path'
 # Define the PostgreSQL connection details
 db_config = {
     'user': 'your_username',
@@ -14,8 +16,12 @@ db_config = {
 engine = create_engine(f"postgresql+psycopg2://{db_config['user']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database']}")
 
 # Step 1: Extract the CSV file
-df = pd.read_csv('superstore.csv', encoding='ISO-8859-1')
-
+try:
+    df = pd.read_csv(file_path, encoding='ISO-8859-1')
+    print("CSV file loaded successfully.")
+except Exception as e:
+    print(f"Error loading CSV file: {e}")
+    
 # Step 2: Transformation
 print(df.columns)  # To check the column names
 df.columns = df.columns.str.strip()  # Strip any leading/trailing whitespace
@@ -25,6 +31,9 @@ columns_to_drop = ['id', 'Country', 'Customer_id', 'Product_id', 'user_id']
 existing_columns = df.columns.intersection(columns_to_drop)
 df.drop(existing_columns, axis=1, inplace=True)
 
+if 'Order_Date' in df.columns:
+    df['Order_Date'] = pd.to_datetime(df['Order_Date'], errors='coerce')
+    
 # Step 3: Load
 # Automatically infer the table schema based on the CSV file
 metadata = MetaData()
@@ -49,6 +58,9 @@ table = Table(table_name, metadata, *columns)
 
 metadata.create_all(engine)
 
-df.to_sql(table_name, engine, if_exists='replace', index=False)
+try:
+    df.to_sql(table_name, engine, if_exists='replace', index=False)
+    print(f"Data loaded into table {table_name} successfully!")
+except SQLAlchemyError as e:
+    print(f"Error loading data into PostgreSQL: {e}")
 
-print(f"Data loaded into table {table_name} successfully!")
